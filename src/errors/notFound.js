4763bf83821a4db9c330843ee7e1aacce86328e9
Zechariah Dominguez <zechariahd@gmail.com>
2022-03-03 17:03:56 -0500

    deleted dupplicate files

--- src/errors/notFound.js
+++
@@ -1,5 +0,0 @@
-function notFound(request, response, next) {
-  next({ status: 404, message: `Path not found: ${request.originalUrl}` });
-}
-
-module.exports = notFound;
--- src/utils/nextId.js
+++
@@ -1,7 +0,0 @@
-const crypto = require("crypto");
-
-function nextId() {
-  return crypto.randomBytes(16).toString("hex");
-}
-
-module.exports = nextId;
